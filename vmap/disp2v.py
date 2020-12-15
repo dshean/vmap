Correct masking of static surfaces

vmap/disp2v.py
@@ -256,7 +256,7 @@ def main():
             #TODO:
             # Probably best to predefine mask over static low sloped area and pass via mask_fn option
             mask_list = args.mask_list
-            mask = dem_mask.get_mask(src_ds,mask_list=['glaciers'],dem_fn=src_fn)
+            mask = dem_mask.get_mask(src_ds,mask_list=mask_list,dem_fn=src_fn)
         else:
             print("\nWarping input raster mask")
             #This can be from previous dem_mask.py run (e.g. *rockmask.tif)
@@ -273,11 +273,11 @@ def main():
 
         print("\nRemoving median x and y offset over static control surfaces")
         h_myr_count = h_myr.count()
-        h_myr_static_count = h_myr[mask].count()
-        h_myr_med = malib.fast_median(h_myr[mask])
-        v_myr_med = malib.fast_median(v_myr[mask])
-        h_myr_mad = malib.mad(h_myr[mask])
-        v_myr_mad = malib.mad(v_myr[mask])
+        h_myr_static_count = np.ma.array(h_myr,mask=mask).count()
+        h_myr_med = malib.fast_median(np.ma.array(h_myr,mask=mask))
+        v_myr_med = malib.fast_median(np.ma.array(v_myr,mask=mask))
+        h_myr_mad = malib.mad(np.ma.array(h_myr,mask=mask))
+        v_myr_mad = malib.mad(np.ma.array(v_myr,mask=mask))
         print("Static pixel count: %i (%0.1f%%)" % (h_myr_static_count, 100*float(h_myr_static_count)/h_myr_count))
         print("median (+/-NMAD)")
         print("x velocity offset: %0.2f (+/-%0.2f) m/%s" % (h_myr_med, h_myr_mad, t_unit))